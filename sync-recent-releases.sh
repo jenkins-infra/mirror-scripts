fd18da0ecef1ae2216f935eb64f2a3a24554b6f6
mirrorbrain Puppet-managed User <mirrorbrain@ip-172-31-33-96.ec2.internal>
2020-08-22 10:15:38 +0000

    Update timestamp as well

--- sync-recent-releases.sh
+++ sync-recent-releases.sh
@@ -26,5 +26,6 @@ while IFS= read -r release; do
     blobxfer upload --storage-account "$AZURE_STORAGE_ACCOUNT" --storage-account-key "$AZURE_STORAGE_KEY" --local-path "${BASE_DIR}/plugins/$release" --remote-path mirrorbits/plugins/${release} --recursive --mode file --no-overwrite --exclude 'mvn%20org.apache.maven.plugins:maven-release-plugin:2.5:perform' --file-md5 --skip-on-md5-match  --no-progress-bar
      rsync -avz ${BASE_DIR}/plugins/${release} www-data@archives.jenkins-ci.org:/srv/releases/plugins/${release}
      rsync -avz ${BASE_DIR}/plugins/${release} ${HOST}:jenkins/plugins/${release}
+     ssh ${HOST} 'echo $(date +%s) > jenkins/TIME'
 done <<< "${RECENT_RELEASES}"
 
